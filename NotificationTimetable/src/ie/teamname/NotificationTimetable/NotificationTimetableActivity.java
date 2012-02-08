13fd9f2dbeffce8560ab9ce96aae29ee3a8e9dca
Don <darrenoneill123@gmail.com>
2012-02-08 15:11:14 +0000

    test NT 1
    commit from package explorer

---
+++ NotificationTimetable/src/ie/teamname/NotificationTimetable/NotificationTimetableActivity.java
@@ -0,0 +1,14 @@
+package ie.teamname.NotificationTimetable;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NotificationTimetableActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        int i = 0;
+    }
+}
